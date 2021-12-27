29ce06552eed120f9d6fe80aa177f52ccc3cac7c
denggang <75230519@qq.com>
2021-12-27 16:24:03 +0800

    初始化提交

---
+++ src/main/java/com/usian/Demo01.java
@@ -0,0 +1,13 @@
+package com.usian;
+
+/**
+ * @作者: Steel.D虫洞时空
+ * @时间: 2021-12-27 16:19
+ * @版本 1.0
+ * 祖师爷保佑，永无Bug
+ */
+public class Demo01 {
+    public static void main(String[] args) {
+
+    }
+}
